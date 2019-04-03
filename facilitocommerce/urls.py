a78a60867bce91f0d31fec92dcb9f2b1ccb811b8
Eduardo <eduardo78d@gmail.com>
2019-04-03 14:13:25 -0600

    Form HTML

--- facilitocommerce/urls.py
+++ facilitocommerce/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 
-from .views import home_page
+from .views import home, login
 
 urlpatterns = [
-    path('', home_page),
+    path('', home),
+    path('login/', login),
     path('admin/', admin.site.urls),
 ]
--- facilitocommerce/views.py
+++ facilitocommerce/views.py
@@ -1,6 +1,26 @@
 from django.shortcuts import render
 #from django.http import HttpResponse
+#return HttpResponse('Hola mundo')
 
-def home_page(request):
-    #return HttpResponse('Hola mundo')
-    return render(request, 'home_page.html', {})
+def home(request):
+    context = {
+        'title': 'Listado de productos',
+        'message': 'Productos',
+        'products': [
+            {'title': 'Playeras', 'stock': True},
+            {'title': 'Tazas', 'stock': True},
+            {'title': 'Art toy', 'stock': True},
+            {'title': 'Peluches', 'stock': False},
+        ]
+    }
+    return render(request, 'home_page.html', context)
+
+def login(request):
+    context = {
+        'title': 'Login'
+    }
+    if request.method == 'POST':
+        print(request.POST)
+        print(request.POST.get('username'))
+        
+    return render(request, 'login.html', context)
